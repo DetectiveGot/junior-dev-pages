531a46a15959f874e0ab4105f316c495f9680bb6
Paphangkon <paphangkon009@gmail.com>
2025-12-23 20:40:06 +0700

    update tailwindcss, redesign home, add uis, lib, components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased 
+          bg-[url(/images/background.svg)] 
+        `}
       >
         {children}
       </body>
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,35 +2,13 @@
 
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
-import "../styles/footer_styles.css";
 
 export default function Footer() {
-    const [theme, setTheme] = useState(() => {
-        if (typeof window !== "undefined") {
-            return document.body.getAttribute("data-theme") || "light";
-        }
-        return "light"; 
-    });
-
-    useEffect(() => {
-        document.body.setAttribute("data-theme", theme);
-    }, [theme]);
-
-    const toggleTheme = () => {
-        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
-    };
-
     return (
-        <footer className="Footer">
-            <center className="text-footer">
-                <Link href="/">© 2024 Junior Dev. All rights reserved.</Link>
-            </center>
-            <div className="themebutton">
-                {/* Toggle Theme:  */}
-                <button className="theme-toggle-btn" onClick={toggleTheme}>
-                    {/* {theme === "light" ? "   🌙 Dark Mode   " : "   ☀️ Light Mode   "} */}
-                </button>
-            </div>
+        <footer>
+            <Link href='/'>
+                <p className="text-sm">© 2024 Junior Dev. All rights reserved.</p>
+            </Link>
         </footer>
     );
 }
